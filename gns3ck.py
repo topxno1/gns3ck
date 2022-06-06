2ad85dcc80cb4a1318975371c8a3a6d967495e88
topxno1 <topxno1@gmail.com>
2022-06-06 16:30:10 +0800

    修改輸出

--- gns3ck.py
+++ gns3ck.py
@@ -3,8 +3,8 @@ import requests
 try:
     r = requests.get("http://120.125.82.173", timeout=5)
     if r.status_code==200:
-        print("Response OK!")
+        print("有連通")
     else:
-        print("Response Error!")
+        print("無回應")
 except:
-    print("No Response")
+    print("無回應")
