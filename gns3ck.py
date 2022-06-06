49fa8861d167b6e014914151fa7d509f30be026b
topxno1 <topxno1@gmail.com>
2022-06-06 16:26:54 +0800

    add try/except

--- gns3ck.py
+++ gns3ck.py
@@ -1,5 +1,10 @@
 import requests
 
-r = requests.get("http://120.125.82.173", timeout=5)
-
-print(r.status_code)
+try:
+    r = requests.get("http://120.125.82.173", timeout=5)
+    if r.status_code==200:
+        print("Response OK!")
+    else:
+        print("Response Error!")
+except:
+    print("No Response")
