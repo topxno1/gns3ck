12b3c29ebc076e5c7cd9158c35241fa38b76ad68
topxno1 <topxno1@gmail.com>
2022-06-12 17:45:42 +0800

    add log

--- gns3ck.py
+++ gns3ck.py
@@ -1,5 +1,10 @@
 import requests
-url = ""
+import string
+import time
+
+url = "http://120.125.82.173"
+#url = "http://120.125.82.155"
+#url = "https://docs.python.org/zh-tw/3/library/tisdfsdf"
 
 try:
     r = requests.get(url, timeout=5)
@@ -7,5 +12,11 @@ try:
         print("有連通")
     else:
         print("無回應")
-except:
-    print("無回應")
+        f=open('log.txt','a')
+        f.write(time.ctime()+" HTTP Error "+str(r.status_code)+"\n")
+        f.close()
+except Exception as e:
+    print("無連通")
+    f=open('log.txt','a')
+    f.write(time.ctime()+" "+str(e)+"\n")
+    f.close()
